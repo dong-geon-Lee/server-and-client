8cd2c9d0574ffa712e357eb6af205d7b3eea3bc6
dong-geon-Lee <ch9716@naver.com>
2022-03-12 17:10:47 +0900

    REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 8000;
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("../backend/routes/goalRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
