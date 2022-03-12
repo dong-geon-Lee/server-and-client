8f6575e394e839b200be7080d5258c98f25d6692
dong-geon-Lee <ch9716@naver.com>
2022-03-12 23:06:25 +0900

    re

--- frontend/README.md
+++ frontend/README.md
@@ -8,3 +8,7 @@
 8. proxy 등록하기 http.. 5000
 9. toasify 설치하고 App.js css와 컴포넌트 container 넣기
 10. Register 컴포넌트에 authSlice 액션 함수 불러오기 register,reset
+11. useEffect 사용해서 종속성 넣고 toastify 사용하기.
+    useSelector의 변수를 종속성으로 넣고 마지막 reset dispatch 하기 isSuccess:true -> false로 바꿔주기
+12. logout 까지 만들어 준다음에 Header로 가서 user의 유무로 삼항 연산자 나열하기
+13. Login createAsyncThunk 작업하기
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -37,6 +37,10 @@ export const register = createAsyncThunk(
   }
 );
 
+export const logout = createAsyncThunk("auth/logout", async () => {
+  localStorage.removeItem("user");
+});
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -62,6 +66,9 @@ export const authSlice = createSlice({
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
+        state.user = null;
+      })
+      .addCase(logout.fulfilled, (state) => {
         state.user = null;
       });
   },
